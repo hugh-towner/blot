dace8bf3831692fcf7ca25da075f469118ba31d5
Hugh <hugh.towner@gmail.com>
2024-06-25 16:29:00 +0100

    more work on input parser to generate a graph output

--- inputProcessor.js
+++ inputProcessor.js
@@ -12,7 +12,9 @@
 
 
 let sampleInput = `A([start]) -->|Get money| B[Go shopping]
-                    B --> C{Let me think}`;
+                    B --> C{Let me think}
+                    C -->|One| D[/output/]         
+                    C -->|two| E[/output two/]`;
 
 
 input = sampleInput.split('\n').map((line) => line.trim());
@@ -24,9 +26,8 @@ function getArrowLabel(line) {
     let end = line.indexOf('|', start + 1);
     let newLine = line.slice(0, start) + line.slice(end + 1);
     let arrowLabel = line.slice(start + 1, end);
-    console.log(arrowLabel);
 
-    return newLine;
+    return [newLine, arrowLabel];
 }
 
 function getElementType(element) {
@@ -41,26 +42,79 @@ function getElementType(element) {
     }
 }
 
-function extractIDandLabel(element) {
-    let start = element.indexOf('(') || element.indexOf('[') || element.indexOf('{');
-    let end = element.indexOf(')') || element.indexOf(']') || element.indexOf('}');
-    let label = element.slice(start + 1, end);
 
-    return {id: element.slice(0, start), label: label};
+function extractIDandLabel(element,type) {
     
+    let start = element.indexOf('[');
+    let end = element.indexOf(']');
+
+    switch (type) {
+        case 'io':
+            return {
+                id: element.slice(0, start),
+                label: element.slice(start + 2, end - 1)
+            };
+        case 'start':
+            return {
+                id: element.slice(0, start - 1),
+                label: element.slice(start + 1, end)
+            };
+        case 'decision':
+            start = element.indexOf('{');
+            end = element.indexOf('}');
+            return {
+                id: element.slice(0, start),
+                label: element.slice(start + 1, end)
+            };
+        case 'process':
+            return {
+                id: element.slice(0, start),
+                label: element.slice(start + 1, end)
+            };
+        default:
+            return null;
+    }
+}
+
+function extractElement(element) {
+    let elementType = getElementType(element);
+        if (elementType) {
+            let { id, label } = extractIDandLabel(element, elementType);
+            return { id, label, type: elementType};
+        } else {
+            return { id: element, type: 'id'};
+        }
 }
 
 let output = input.map((line) => {
+    let arrowLabel = null;
     if (line.includes('|')) {
-        line = getArrowLabel(line);
+        [line, arrowLabel] = getArrowLabel(line);
     }
 
+    console.log(line)
     linkedElements = line.split('-->').map((element) => element.trim());
-    console.log(linkedElements);
-    for (let i = 0; i < linkedElements.length; i++) {
-        let elementType = getElementType(linkedElements[i]);
-        console.log(elementType);
-        console.log(extractIDandLabel(linkedElements[i]));
-    }
-    console.log(line);
+    let arrow = {label: arrowLabel, start: null, end: null};
+
+    arrow.start = extractElement(linkedElements[0]);
+    arrow.end = extractElement(linkedElements[1]);
+
+    return arrow;
 })
+
+console.log(output);
+
+
+let graph = {};
+
+output.forEach((arrow) => {
+    let { start, end } = arrow;
+
+    if (graph[start.id]) {
+        graph[start.id].push(end);
+    } else {
+        graph[start.id] = [end];
+    }
+})
+
+console.log(graph);
